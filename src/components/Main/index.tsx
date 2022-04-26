4aaf2484c68b9a2ff75668a60e94c8403d202086
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-04-26 23:43:55 +0100

    first commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,3 @@
-import * as S from './styles'
-
-const Main = () => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um atomo e React Avancado escrito ao lado"
-    />
-    <S.Title>react avancado</S.Title>
-    <S.Description>
-      Typescript, ReactJs, NextJs e Styled Components
-    </S.Description>
-    <S.Illustraction
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com codigo"
-    />
-  </S.Wrapper>
-)
+const Main = () => <h1>NexJs Na pratica</h1>
 
 export default Main
